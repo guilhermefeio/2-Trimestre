6c7d5584286349245dcad9ffcd8495d8bf3cb627
Guigostoso <148064776+guilhermefeio@users.noreply.github.com>
2024-07-31 07:54:58 -0300

    .

---
+++ main.js
@@ -0,0 +1,17 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Quem você mais gosta?",
+        alternativas: [
+            {
+          
+            }
+        ]
+
+    }
+]
